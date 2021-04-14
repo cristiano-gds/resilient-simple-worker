4220a3c54ee173441dccadb3d407ddfe2f9b62f9
Cristiano Gomes <cristianogds@gmail.com>
2021-04-13 21:30:37 -0300

    first commit

---
+++ src/webapi/Controllers/FileController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileController : ControllerBase
+    {
+        private const string PATHFILE = "./files/simple-file.txt";
+        private readonly ILogger<FileController> _logger;
+
+        public FileController(ILogger<FileController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            if (System.IO.File.Exists(PATHFILE))
+                return BadRequest();
+
+            System.IO.File.WriteAllText(PATHFILE, $"[{DateTime.Now.ToString()}] Arquivo criado");
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            if(!System.IO.File.Exists(PATHFILE))
+                return BadRequest();
+
+            System.IO.File.Delete(PATHFILE);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] string message)
+        {
+            if(!System.IO.File.Exists(PATHFILE))
+                return BadRequest();
+
+            System.IO.File.WriteAllText(
+                PATHFILE,
+                $"[{DateTime.Now.ToString()}] {message}");
+            return Ok();
+        }
+    }
+}
---
+++ src/worker/Worker.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Polly;
+
+namespace worker
+{
+    public class Worker : BackgroundService
+    {
+        private const string PATHFILE = "./files/simple-file.txt";
+        private DateTime lastChangeInFile = DateTime.MinValue;
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+                //Polly policy: automatic retries if occurs specific error 
+                var retry = Policy.Handle<FileNotFoundException>()
+                    .WaitAndRetry(
+                        5, //number of retries
+                        t => TimeSpan.FromSeconds(10), //interval between retries
+                        onRetry: (exception, TimeSpan, retryCount, context) => {
+                            Console.WriteLine("Attempt to read file #{0}", retryCount);
+                        }
+                    );
+
+                //Polly policy: execute a alternative script block if occurs specific error
+                var fallback = Policy.Handle<FileNotFoundException>()
+                    .Fallback(() => {
+                        Console.WriteLine("Attempt exceeded, process ignored");
+                    });
+
+                //Polly policy: combine policies
+                var wrap = Policy.Wrap(fallback, retry);
+
+                wrap.Execute(() => {
+                    
+                    DateTime lastWriteTime;
+                    
+                    if((lastWriteTime = File.GetLastWriteTime(PATHFILE)) != lastChangeInFile)
+                    {
+                        Console.WriteLine("New content in file:{0}", File.ReadAllText(PATHFILE));
+                        lastChangeInFile = lastWriteTime;
+                    }
+                });
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
